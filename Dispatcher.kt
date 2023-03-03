afb4abdbd339a6f32cabfafdcc89aa089924ce2f
AbdulazimovRuslan <66431535+Ruslan5787@users.noreply.github.com>
2023-03-03 13:02:39 +0500

    Delete Dispatcher.kt

--- Dispatcher.kt
+++
@@ -1,8 +0,0 @@
-import MotorDepot.MotorDepot
-
-open class Dispatcher(driverName: String, flightName: String, carName: String, startFlight: String, endFlight: String): MotorDepot(driverName, flightName, carName, startFlight, endFlight) {
-
-    open fun suspensionDriver() {
-        println("Водитель ${driverName} на рейсе ${flightName} на ${carName} отстранен.");
-    }
-}
